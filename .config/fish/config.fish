fdc979549793f3361884087ec62b9d676354dd03
TheAsel <a.osele@proton.me>
2025-08-30 19:01:57 +0200

    abbr > alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,35 +17,35 @@ if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
     cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
 end
 
-alias pamcan pacman
-alias clear "printf '\033[2J\033[3J\033[1;1H'"
-alias q 'qs -c ii'
+abbr pamcan pacman
+abbr clear "printf '\033[2J\033[3J\033[1;1H'"
+abbr q 'qs -c ii'
 
 # # Helpful aliases
-alias c="printf '\033[2J\033[3J\033[1;1H'"                                # clear terminal
-alias l='eza -lh --icons=auto'                                            # long list
-alias ls='eza -1 --icons=auto'                                            # short list
-alias ll='eza -lha --icons=auto --sort=name --group-directories-first'    # long list all
-alias ld='eza -lhD --icons=auto'                                          # long list dirs
-alias lt='eza --icons=auto --tree'                                        # list folder as tree
-alias in='yay -S'                                                         # install package
-alias un='yay -Rns'                                                       # uninstall package
-alias up='yay -Syu && flatpak update'                                     # update system/package/aur/flatpak
-alias pl='yay -Qs'                                                        # list installed package
-alias pa='yay -Ss'                                                        # list available package
-alias pc='yay -Sc'                                                        # remove unused cache
-alias po='yay -Qtdq | xargs -r -o yay -Rns && flatpak uninstall --unused' # remove unused packages
-alias vc='code'                                                           # gui code editor
+abbr c "printf '\033[2J\033[3J\033[1;1H'"                                  # clear terminal
+abbr l 'eza -lh --icons=auto'                                              # long list
+abbr ls 'eza -1 --icons=auto'                                              # short list
+abbr ll 'eza -lha --icons=auto --sort=name --group-directories-first'      # long list all
+abbr ld 'eza -lhD --icons=auto'                                            # long list dirs
+abbr lt 'eza --icons=auto --tree'                                          # list folder as tree
+abbr in 'paru -S'                                                          # install package
+abbr un 'paru -Rns'                                                        # uninstall package
+abbr up 'paru -Syu && flatpak update'                                      # update system/package/aur/flatpak
+abbr pl 'paru -Qs'                                                         # list installed package
+abbr pa 'paru -Ss'                                                         # list available package
+abbr pc 'paru -Sc'                                                         # remove unused cache
+abbr po 'paru -Qtdq | xargs -r -o paru -Rns && flatpak uninstall --unused' # remove unused packages
+abbr vc 'code'                                                             # gui code editor
 
 # # Directory navigation shortcuts
-alias ..='cd ..'
-alias ...='cd ../..'
-alias .3='cd ../../..'
-alias .4='cd ../../../..'
-alias .5='cd ../../../../..'
+abbr .. 'cd ..'
+abbr ... 'cd ../..'
+abbr .3 'cd ../../..'
+abbr .4 'cd ../../../..'
+abbr .5 'cd ../../../../..'
 
 # # Always mkdir a path (this doesn't inhibit functionality to make a single dir)
-alias mkdir='mkdir -p'
+abbr mkdir 'mkdir -p'
     
 
 # function fish_prompt

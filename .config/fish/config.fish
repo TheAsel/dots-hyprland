6f56d075fc8347efb5a72e616bbfac8a43e20954
TheAsel <a.osele@proton.me>
2025-09-03 17:27:56 +0200

    fix config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,6 +15,7 @@ if status is-interactive # Commands to run in interactive sessions can go here
     starship init fish | source
     if test -f ~/.local/state/quickshell/user/generated/terminal/sequences.txt
         cat ~/.local/state/quickshell/user/generated/terminal/sequences.txt
+    end
 
     # Aliases
     abbr pamcan pacman

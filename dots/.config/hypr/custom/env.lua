479ce92e8016c67849bdba440a052d6737395d03
TheAsel <a.osele@proton.me>
2026-05-15 15:21:07 +0200

    Update env.lua

--- dots/.config/hypr/custom/env.lua
+++ dots/.config/hypr/custom/env.lua
@@ -1,2 +1,5 @@
 hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "24")

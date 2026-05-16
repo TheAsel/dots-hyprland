77548a2e617bdc7a89623290460e12b4af4b1cd4
TheAsel <a.osele@proton.me>
2026-05-16 19:11:52 +0200

    Update execs.lua

--- dots/.config/hypr/custom/execs.lua
+++ dots/.config/hypr/custom/execs.lua
@@ -1,7 +1,7 @@
 hl.on("hyprland.start", function()
 
-	hl.exec_cmd("keepassxc --minimized")
-	hl.exec_cmd("QT_QPA_PLATFORM=xcb synology-drive start")
-	hl.exec_cmd("trayscale --hide-window")
+	hl.exec_cmd("sleep 3 && keepassxc --minimized")
+	hl.exec_cmd("sleep 3 && QT_QPA_PLATFORM=xcb synology-drive start")
+	hl.exec_cmd("sleep 3 && trayscale --hide-window")
 
 end)
